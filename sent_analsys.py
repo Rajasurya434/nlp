45eaed6bcaa6940b9a7eff358c2293818debf92b
Raja Surya <rajasurya26999@gmail.com>
2025-01-28 00:56:55 +0530

    updated code and output pics

--- sent_analsys.py
+++ sent_analsys.py
@@ -1,11 +1,12 @@
 from transformers import pipeline
 
 
-sentiment_analyzer = pipeline("sentiment-analysis", return_all_scores=True)
+sentiment_analyzer = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english", return_all_scores=True)
 
 def analyze_sentiment(text):
     result = sentiment_analyzer(text)
     return result[0]  
+
 
 if __name__ == "__main__":
     user_input = input("Enter text for sentiment analysis: ")
